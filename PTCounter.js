8b75895290b7c7fc9b751b5a80feaff66622e865
Semyon Valyaev <semyon.valyaev@gamil.com>
2022-08-15 20:39:36 +0300

    Added config files (vs-code, prettier)

--- PTCounter.js
+++ PTCounter.js
@@ -1,15 +1,15 @@
-"use strict";
-const fs = require("fs");
-const gcodeRegExp = /\.gcode/;
-let gcodeFiles = new Set();
-fs.readdir("./", "utf-8", (error, files) => {
+'use strict'
+const fs = require('fs')
+const gcodeRegExp = /\.gcode/
+let gcodeFiles = new Set()
+fs.readdir('./gcode/', 'utf-8', (error, files) => {
   for (const file of files) {
     if (gcodeRegExp.test(file)) {
-      console.log(typeof file);
-      gcodeFiles.add(file.toString());
+      console.log(typeof file)
+      gcodeFiles.add(file.toString())
     }
   }
-});
+})
 setTimeout(() => {
-  console.log(gcodeFiles);
-}, 10);
+  console.log(gcodeFiles)
+}, 10)
